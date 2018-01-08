ff42e9eed16be3f2f6aaaf193ec172194b4bbd40
Dennis Marchetti <dmarchet84@gmail.com>
2018-01-08 14:22:36 -0600

    add todos to home class, display on page when added to list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,10 +10,18 @@ export class HomeComponent implements OnInit {
   itemCount: number = 4;
   btnText: string = 'Add an Item';
   todoText: string= 'My first thing to do';
+  todos = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.todos.length;
+  }
+
+  addItem() {
+    this.todos.push(this.todoText);
+    this.todoText = '';
+    this.itemCount = this.todos.length;
   }
 
 }
